c953d2ac6db7b1c10991101bb78403a9f5ff914f
PankajSon <psen74275@gmail.com>
2023-05-03 19:24:12 +0530

    second

---
+++ may3.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+
+//functions
+
+/* comment
+
+multi line
+
+*/
+int i=300;
+int main(){
+
+cout<<i<<endl;
+int i=251;
+cout<<i<<endl;
+int count=0;
+int n;
+cout<<"enter your number";
+cin>>n;
+    for(i;i>=n;i--){
+        cout<<i<<" ";
+    count++;
+    }
+    cout<<"count will be"<<count<<endl;
+cout<<i<<endl;
+cout<<::i<<endl;
+
+
+    return 0;;
+}
+
---
+++ power.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+
+int base;
+int exponent;
+cout<<"enter the base value"<<endl;
+cin>>base;
+cout<<"enter the exponent"<<endl;
+cin>>exponent;
+
+return 0;
+
+
+}
