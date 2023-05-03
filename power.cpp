5fd00545a99d186738854ce5e20c298f6fe45cd6
PankajSon <psen74275@gmail.com>
2023-05-03 20:05:14 +0530

    power

--- power.cpp
+++ power.cpp
@@ -2,15 +2,18 @@
 #include<math.h>
 using namespace std;
 int main(){
-
-int base;
-int exponent;
+double answer=1;
+double base;
+double exponent;
 cout<<"enter the base value"<<endl;
 cin>>base;
 cout<<"enter the exponent"<<endl;
 cin>>exponent;
-
+// answer=pow(base,exponent);
+// cout<<answer;
+ for(int i=1;i<=exponent;i++){
+     answer= answer*base;
+ }
+ cout<<answer;
 return 0;
-
-
 }
