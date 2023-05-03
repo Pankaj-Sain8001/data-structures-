ce10cd873a6d757789ede93faff92539753c31ba
PankajSon <psen74275@gmail.com>
2023-05-03 20:57:36 +0530

    recursion

--- power.cpp
+++ power.cpp
@@ -1,19 +1,21 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
-int main(){
-double answer=1;
-double base;
-double exponent;
-cout<<"enter the base value"<<endl;
-cin>>base;
-cout<<"enter the exponent"<<endl;
-cin>>exponent;
-// answer=pow(base,exponent);
-// cout<<answer;
- for(int i=1;i<=exponent;i++){
-     answer= answer*base;
- }
- cout<<answer;
-return 0;
+int main()
+{
+    double answer = 1;
+    double base;
+    double exponent;
+    cout << "enter the base value" << endl;
+    cin >> base;
+    cout << "enter the exponent" << endl;
+    cin >> exponent;
+    answer = pow(base, exponent);
+    cout << answer;
+    for (int i = 1; i <= exponent; i++)
+    {
+        answer = answer * base;
+    }
+    cout << answer;
+    return 0;
 }
